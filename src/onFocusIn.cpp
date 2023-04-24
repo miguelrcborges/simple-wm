8057a3248a55dd4700f2798ef67d1b48cd32879a
Miguel Borges <up202004481@edu.fe.up.pt>
2023-04-24 22:01:05 +0100

    remove iostream on onFocusIn :skull:

--- src/onFocusIn.cpp
+++ src/onFocusIn.cpp
@@ -1,7 +1,5 @@
 #include "globals.h"
 
-#include <iostream>
-
 void onFocusIn(const XFocusChangeEvent &event) {
 	for (size_t i = 0; i < monitors[active_monitor].windows.size(); ++i) {
 		if (monitors[active_monitor].windows[i].win == event.window) {
